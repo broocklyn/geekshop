7e5b1b145528a43ebfac0bd90ef9776318972d1f
broocklyn <d.broocklyn@gmail.com>
2019-07-23 23:57:12 +0300

    urls fix

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,6 +20,6 @@ import mainapp.views as mainapp
 urlpatterns = [
     re_path(r'^$', mainapp.index),
     re_path(r'^products/$', mainapp.products),
-    re_path(r'^contact/$', mainapp.index),
+    re_path(r'^contact/$', mainapp.contact),
     path('admin/', admin.site.urls),
 ]

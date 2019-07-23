de7bbcb97a4cff7e5911c7185dcddd18834099e0
broocklyn <d.broocklyn@gmail.com>
2019-07-23 23:50:36 +0300

    added views

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainapp/index.html')
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')

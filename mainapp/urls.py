aa38d8c8008ddb6f9f408425bdff7dbb16332ec8
broocklyn <d.broocklyn@gmail.com>
2019-07-28 22:38:38 +0300

    edited include menu, fixed namespace

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'mainapp'
 urlpatterns = [
     re_path(r'^$', mainapp.index, name='index'),
     re_path(r'^products/$', mainapp.products, name='products'),
-    re_path(r'^contact/$', mainapp.contact, name='contact'),
+    re_path(r'^contact/$', mainapp.contact, name='contact')
 ]

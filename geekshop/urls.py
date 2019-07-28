18fd04730c3a489e73cee46bbe159ad195ce74a5
broocklyn <d.broocklyn@gmail.com>
2019-07-28 19:31:47 +0300

    urls added

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, re_path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    re_path(r'^$', mainapp.index),
-    re_path(r'^products/$', mainapp.products),
+    re_path(r'^$', mainapp.index, name='index'),
+    re_path(r'^products/$', mainapp.products, name='products'),
     re_path(r'^contact/$', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]

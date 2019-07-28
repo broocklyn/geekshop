89c7887041daece5577b355c71ed04301eb35136
broocklyn <d.broocklyn@gmail.com>
2019-07-28 20:34:12 +0300

    added locations to views

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,6 +9,27 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 def contact(request):
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': '+7-495-468-20-35',
+            'e-mail': 'info@geekshop.ru',
+            'address': 'В пределах МКАД'
+        },
+        {
+            'city': 'Санкт-Петербург',
+            'phone': '+7-812-327-20-25',
+            'e-mail': 'info@spb.geekshop.ru',
+            'address': 'В пределах КАД'
+        },
+        {
+            'city': 'Краснодар',
+            'phone': '+7-831-188-13-46',
+            'e-mail': 'info@krd.geekshop.ru',
+            'address': 'В пределах края'
+        }
+
+    ]
     context = {
         'page_title': 'Контакты',
     }

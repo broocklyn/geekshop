616a565f3e833c694daaedefb2854ed0ab5699e1
broocklyn <d.broocklyn@gmail.com>
2019-07-30 21:45:44 +0300

    models.py edited, added class product

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,3 +5,16 @@ class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя категории', max_length=128, unique=True)
 
     description = models.TextField(verbose_name='описание', blank=True, null=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image =  models.ImageField(upload_to='products_images', blank=True)
+    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+    def __str__(self):
+        return  "{}  ({})".format(self.name, self.category.name)

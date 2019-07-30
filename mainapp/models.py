09fb7066fad038536801acf01573d07560cb7102
broocklyn <d.broocklyn@gmail.com>
2019-07-30 21:52:40 +0300

    models.py edited

--- mainapp/models.py
+++ mainapp/models.py
@@ -10,11 +10,11 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
-    image =  models.ImageField(upload_to='products_images', blank=True)
+    image = models.ImageField(upload_to='products_images', blank=True)
     short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
     description = models.TextField(verbose_name='описание продукта', blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
 
     def __str__(self):
-        return  "{}  ({})".format(self.name, self.category.name)
+        return f"{self.name} ({self.category.name})"

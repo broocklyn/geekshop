671b2467eaaf6d3a100afeff52842fcc05f5f28d
broocklyn <d.broocklyn@gmail.com>
2019-07-30 21:16:45 +0300

    models.py edited

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя категории', max_length=128, unique=True)
+
+    description = models.TextField(verbose_name='описание', blank=True, null=True)

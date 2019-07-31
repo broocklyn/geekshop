53112a43b5c78766aea61f4f555538f876b4e7b6
broocklyn <d.broocklyn@gmail.com>
2019-07-31 21:17:14 +0300

    fixed mistake

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mainapp.models import ProductCategory
+from mainapp.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)

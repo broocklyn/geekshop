a4ab2dc3478e46bac64d438ccc892d8db6f6178b
broocklyn <d.broocklyn@gmail.com>
2019-07-31 21:23:32 +0300

    models optimizing

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя категории', max_length=128, unique=True)
     description = models.TextField(verbose_name='описание', blank=True, null=True)
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Product(models.Model):

61cd92551cd331f8b730e0f38bcefc3eaf23e63b
broocklyn <d.broocklyn@gmail.com>
2019-08-03 18:08:45 +0300

    some fixes

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,8 +1,12 @@
+import os
+import json
+
 from django.core.management.base import BaseCommand
 from mainapp.models import ProductCategory, Product
 from django.contrib.auth.models import User
+from django.conf import settings
 
-import json, os
+
 
 
 

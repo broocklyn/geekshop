7d750cf411aff6a61c1de3f33bb7f00cbc952ab8
broocklyn <d.broocklyn@gmail.com>
2019-08-03 22:19:30 +0300

    added form

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def login(request):
+    form = ShopUserLoginForm()
     context = {
-
+        'form': form,
     }
     return render(request, 'authapp/login.html', context)

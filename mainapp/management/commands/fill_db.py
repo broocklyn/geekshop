bb318d77a5682a1eea01b9a366d1f05d958359f9
broocklyn <d.broocklyn@gmail.com>
2019-08-03 18:06:10 +0300

    moved constant json path to settings

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 import json, os
 
-JSON_PATH = 'mainapp/json'
+
 
 def loadFromJSON(file_name):
     with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:

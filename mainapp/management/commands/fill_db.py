c64e716509fa7533ec4b07d0475ebf8ac13dbba7
broocklyn <d.broocklyn@gmail.com>
2019-08-03 19:00:02 +0300

    class Command added

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -14,3 +14,15 @@ def load_from_json(file_name):
     with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
         return json.load(infile)
 
+
+class Command(BaseCommand):
+    help = 'Fill DB new data with deleting old data'
+
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            new_category = ProductCategory(**category)
+            new_category.save()
+

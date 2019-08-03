e1047111d622a58817e5179f7f46941f00a89ff4
broocklyn <d.broocklyn@gmail.com>
2019-08-03 21:47:08 +0300

    forms.py edited

--- authapp/forms.py
+++ authapp/forms.py
@@ -5,3 +5,8 @@ class ShopUserLoginForm(AuthenticationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'password')
+
+        def __init__(self, *args, **kwargs):
+            super(ShopUserLoginForm, self).__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'

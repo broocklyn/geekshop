a595d01787446d24c1556accfe8a12722d02b2d0
broocklyn <d.broocklyn@gmail.com>
2019-08-04 09:44:15 +0300

    view authorization fixes

--- authapp/views.py
+++ authapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 from authapp.forms import ShopUserLoginForm
+from django.contrib import auth
 
 
 def login(request):
     if request.method == 'POST':
         form = ShopUserLoginForm(data=request.POST)
         if form.is_valid():
-            print('OK')
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user. is.active:
+                auth.login(request)
     else:
         form = ShopUserLoginForm()
 

4b255e58493a93cd081068a5e8d5d4769b449861
broocklyn <d.broocklyn@gmail.com>
2019-08-06 22:57:15 +0300

    added user change form

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.forms import forms
 
 from authapp.models import ShopUser
@@ -9,7 +9,7 @@ class ShopUserLoginForm(AuthenticationForm):
         model = ShopUser
         fields = ('username', 'password')
 
-    def __init__(self, *args, **kwargs):                    
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
@@ -24,7 +24,7 @@ class ShopUserRegisterForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-         #   field.help_text = ''
+        #   field.help_text = ''
 
     def clean_age(self):
         data = self.cleaned_data['age']
@@ -34,3 +34,22 @@ class ShopUserRegisterForm(UserCreationForm):
         return data
 
 
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+        def clean_age(self):
+            data = self.cleaned_data['age']
+            if data < 18:
+                raise forms.ValidationError("Вы слишком молоды!")
+
+            return data

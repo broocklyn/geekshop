b5d2327654ee8a0848eab9699eeeb776bfb5c38c
broocklyn <d.broocklyn@gmail.com>
2019-08-07 23:27:26 +0300

    edited urls basketapp

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -5,6 +5,7 @@ app_name = 'basketapp'
 
 urlpatterns = [
     re_path(r'^$', basketapp.index, name='index'),
-
+    re_path('add/(?P<pk>\d+)/', basketapp.basket_add, name='add'),
+    # path('remove/<int:pk>)/', basketapp.basket_remove, name='remove'),
 
 ]

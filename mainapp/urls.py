6b3f327f0fc6c38a74ef3fe5465685cecb5736e2
broocklyn <d.broocklyn@gmail.com>
2019-08-07 22:58:57 +0300

    added alternate url commented

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,7 @@ app_name = 'mainapp'
 urlpatterns = [
     re_path(r'^$', mainapp.index, name='index'),
     re_path(r'^products/$', mainapp.products, name='products'),
-
+    # path('category/<int:pk>/', mainapp.category, name='category'),
     re_path(r'^category/(?P<pk>\d+)$', mainapp.category, name='category'),
 
     re_path(r'^contact/$', mainapp.contact, name='contact')

cf4dddc043cfe45360481bcc35d66c68382606e7
broocklyn <d.broocklyn@gmail.com>
2019-08-07 23:30:27 +0300

    created view

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from mainapp.models import Product
+
+
 def index():
     pass
+
+def basket_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)

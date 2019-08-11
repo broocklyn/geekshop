8316aea63be37442317ac9b8936d3ca2580f0119
broocklyn <d.broocklyn@gmail.com>
2019-08-11 16:56:29 +0300

    added login url

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
@@ -10,6 +11,7 @@ def index():
     pass
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket_item = Basket.objects.filter(user=request.user,

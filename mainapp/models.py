92b60eb90a3e22843aff2377d0a34b05faa0e5fb
broocklyn <d.broocklyn@gmail.com>
2019-08-14 22:52:18 +0300

    edited some models

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя категории', max_length=128, unique=True)
     description = models.TextField(verbose_name='описание', blank=True, null=True)
+    is_active = models.BooleanField(verbose_name='активность', default=True)
     def __str__(self):
         return f"{self.name}"
 
@@ -16,6 +17,7 @@ class Product(models.Model):
     description = models.TextField(verbose_name='описание продукта', blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+    is_active = models.BooleanField(verbose_name='активность', default=True)
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"

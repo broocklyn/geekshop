bc181df4134811f45b4f47bce47220640258b1fb
broocklyn <d.broocklyn@gmail.com>
2019-08-14 22:26:51 +0300

    full user delete feature

--- adminapp/views.py
+++ adminapp/views.py
@@ -50,3 +50,17 @@ def shopuser_update(request, pk):
     }
 
     return render(request, 'adminapp/shopuser_update.html', content)
+
+def shopuser_delete(request, pk):
+    # get_object_or_404(ShopUser, pk=pk).delete()
+    user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('adminapp:index'))
+    elif request.method == 'GET':
+        context = {
+            'page_title': 'админка/удаление пользователя',
+            'object': user,
+        }
+        return render(request, 'adminapp/shopuser_delete.html', context)

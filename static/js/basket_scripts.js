35f12211859652b24c87cd463b09cba554289946
broocklyn <d.broocklyn@gmail.com>
2019-08-14 20:14:06 +0300

    del comments

--- static/js/basket_scripts.js
+++ static/js/basket_scripts.js
@@ -2,9 +2,6 @@ window.onload = function () {
     console.log('DOM LOADED');
     $(".basket-list").on('change', 'input[type="number"]',
         function (event) {
-            // console.log(event.target);
-            // console.log(event.target.name);
-            // console.log(event.target.value);
             $.ajax({
                 url: "/basket/update/" + event.target.name + "/" + event.target.value + "/",
                 success: function (data) {

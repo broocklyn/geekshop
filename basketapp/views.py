717e2a86be0101c441ee44f88cf18144f048df83
broocklyn <d.broocklyn@gmail.com>
2019-08-14 20:25:16 +0300

    inc__basket_list created

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -52,3 +52,4 @@ def basket_update(request, pk, quantity):
         basket_obj = get_object_or_404(Basket, pk=pk)
         print(basket_obj, quantity)
         # return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        return JsonResponse({'result': True})

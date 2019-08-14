74f2ce875e24404eb018b08943d627c709c82245
broocklyn <d.broocklyn@gmail.com>
2019-08-14 21:26:55 +0300

    added adminka to global menu

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from authapp.models import ShopUser
 
 
 def index(request):
+    object_list = ShopUser.objects.all()
+
     context = {
         'page_title': 'админка/пользователи',
+        'object_list': object_list,
 
     }
     return render(request, 'adminapp/index.html', context)

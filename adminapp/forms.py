c76960355496db1e804aead7b690fe5716ad754c
broocklyn <d.broocklyn@gmail.com>
2019-08-14 22:01:10 +0300

    added form adminupdateuser

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -23,22 +23,23 @@ class ShopUserAdminCreateForm(UserCreationForm):
         return data
 
 
-# class ShopUserUpdateForm(UserChangeForm):
-#     class Meta:
-#         model = ShopUser
-#         fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         for field_name, field in self.fields.items():
-#             field.widget.attrs['class'] = 'form-control'
-#             field.help_text = ''
-#             if field_name == 'password':
-#                 field.widget = HiddenInput()
-#
-#     def clean_age(self):
-#         data = self.cleaned_data['age']
-#         if data < 18:
-#             raise forms.ValidationError("Вы слишком молоды!")
-#
-#         return data
+class ShopUserAdminUpdateForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'age', 'avatar', 'password', 'is_superuser',
+                  'is_active')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name == 'password':
+                field.widget = HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+
+        return data

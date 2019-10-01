6002c609d3c274aa994f424fba88234f3721067e
broocklyn <d.broocklyn@gmail.com>
2019-10-01 22:09:22 +0300

    remove regular expressions

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,31 +1,31 @@
 from django.urls import path, re_path
 import adminapp.views as adminapp
 
-app_name = 'adminapp'
+app_name = "adminapp"
 
 urlpatterns = [
     # re_path(r'^$', adminapp.index, name='index'),
-    re_path(r'^$', adminapp.ShopUserListView.as_view(), name='index'),
-    re_path(r'^shopuser/create/$', adminapp.shopuser_create, name='shopuser_create'),
-    re_path(r'^shopuser/update/(?P<pk>\d+)/$', adminapp.shopuser_update, name='shopuser_update'),
-    re_path(r'^shopuser/delete/(?P<pk>\d+)/$', adminapp.shopuser_delete, name='shopuser_delete'),
-    re_path(r'^productcategory/list/$', adminapp.productcategory_list, name='productcategory_list'),
+    path('$', adminapp.ShopUserListView.as_view(), name='index'),
+    path('shopuser/create/', adminapp.shopuser_create, name='shopuser_create'),
+    path('shopuser/update/<int:pk>/', adminapp.shopuser_update, name='shopuser_update'),
+    path('shopuser/delete/<int:pk>/', adminapp.shopuser_delete, name='shopuser_delete'),
+    path('productcategory/list/', adminapp.productcategory_list, name='productcategory_list'),
     # re_path(r'^productcategory/create/$', adminapp.productcategory_create, name='productcategory_create'),
-    re_path(r'^productcategory/create/$', adminapp.ProductCategoryCreateView.as_view(), name='productcategory_create'),
+    path('productcategory/create/', adminapp.ProductCategoryCreateView.as_view(), name='productcategory_create'),
 
     # re_path(r'^productcategory/update/(?P<pk>\d+)/$', adminapp.productcategory_update, name='productcategory_update'),
-    re_path(r'^productcategory/update/(?P<pk>\d+)/$', adminapp.ProductCategoryUpdateView.as_view(),
+    path('productcategory/update/<int:pk>/', adminapp.ProductCategoryUpdateView.as_view(),
             name='productcategory_update'),
 
     # re_path(r'^productcategory/delete/(?P<pk>\d+)/$', adminapp.productcategory_delete, name='productcategory_delete'),
-    re_path(r'^productcategory/delete/(?P<pk>\d+)/$', adminapp.ProductCategoryDeleteView.as_view(),
+    path('productcategory/delete/<int:pk>/', adminapp.ProductCategoryDeleteView.as_view(),
             name='productcategory_delete'),
-    re_path(r'^productcategory/products/(?P<pk>\d+)/$', adminapp.productcategory_products,
+    path('productcategory/products/<int:pk>/', adminapp.productcategory_products,
             name='productcategory_products'),
 
-    re_path(r'^product/create/(?P<pk>\d+)/$', adminapp.product_create, name='product_create'),
-    re_path(r'^product/update/(?P<pk>\d+)/$', adminapp.product_update, name='product_update'),
-    re_path(r'^product/delete/(?P<pk>\d+)/$', adminapp.product_delete, name='product_delete'),
+    path('product/create/(?P<pk>\d+)/', adminapp.product_create, name='product_create'),
+    path('product/update/(?P<pk>\d+)/', adminapp.product_update, name='product_update'),
+    path('product/delete/(?P<pk>\d+)/', adminapp.product_delete, name='product_delete'),
     # re_path(r'^product/read/(?P<pk>\d+)/$', adminapp.product_read, name='product_read'),
-    re_path(r'^product/read/(?P<pk>\d+)/$', adminapp.ProductDetailView.as_view(), name='product_read'),
+    path('product/read/(?P<pk>\d+)/', adminapp.ProductDetailView.as_view(), name='product_read'),
 ]

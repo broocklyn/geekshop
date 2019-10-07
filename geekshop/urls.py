8a92d17560b48ef11a97d645345b1e4990e188e3
broocklyn <d.broocklyn@gmail.com>
2019-10-07 19:14:57 +0300

    added social url

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,18 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     re_path(r'^', include('mainapp.urls', namespace='main')),
     re_path(r'^auth/', include('authapp.urls', namespace='auth')),
     re_path(r'^basket/', include('basketapp.urls', namespace='basket')),
     re_path(r'^myadmin/', include('adminapp.urls', namespace='myadmin')),
+    path('', include('social_django.urls', namespace='social')),
 
     path('admin/', admin.site.urls),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

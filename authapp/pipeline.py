d05f58b3624ceb7c9e1bda230f8502942047695c
broocklyn <d.broocklyn@gmail.com>
2019-10-08 19:49:00 +0300

    edited pipeline.py

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -22,3 +22,24 @@ def save_user_profile(backend, user, response, *args, **kwargs):
                                                 v='5.92')),
                           None
                           ))
+
+    resp = requests.get(api_url)
+    if resp.status_code != 200:
+        return
+
+    data = resp.json()['response'][0]
+    if data['sex']:
+        user.shopuserprofile.gender = ShopUserProfile.MALE if data['sex'] == 2 else ShopUserProfile.FEMALE
+
+    if data['about']:
+        user.shopuserprofile.aboutMe = data['about']
+
+    if data['bdate']:
+        bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+
+        age = timezone.now().date().year - bdate.year
+        if age < 18:
+            user.delete()
+            raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+    user.save()

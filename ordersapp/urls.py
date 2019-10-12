8cdc7f560408ea48391be136cc1b982ed3636b16
broocklyn <d.broocklyn@gmail.com>
2019-10-12 18:53:59 +0300

    edited urls, views

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -4,6 +4,7 @@ import ordersapp.views as ordersapp
 app_name = "ordersapp"
 
 urlpatterns = [
-    re_path(r'^$', ordersapp.index, name='index'),
+    re_path(r'^$', ordersapp.OrderList.as_view(), name='orders_list'),
+
 
 ]

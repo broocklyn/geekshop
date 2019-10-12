32e57737488f0cdc82abbed453569a9c329e3e9f
broocklyn <d.broocklyn@gmail.com>
2019-10-12 18:17:29 +0300

    added class order

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from mainapp.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    FORMING = 'FM'
+    SENT_TO_PROCEED = 'STP'
+    PROCEEDED = 'PRD'
+    PAID = 'PD'
+    READY = 'RDY'
+    CANCEL = 'CNC'
+
+    ORDER_STATUS_CHOICES = (
+        (FORMING, 'формируется'),
+        (SENT_TO_PROCEED, 'отправлен в обработку'),
+        (PAID, 'оплачен'),
+        (PROCEEDED, 'обрабатывается'),
+        (READY, 'готов к выдаче'),
+        (CANCEL, 'отменен'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
+    status = models.CharField(verbose_name='статус', max_length=3, choices=ORDER_STATUS_CHOICES, default=FORMING)
+    is_active = models.BooleanField(verbose_name='активен', default=True)

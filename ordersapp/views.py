cb3717b81dadea3388c20b0900da783383574062
broocklyn <d.broocklyn@gmail.com>
2019-10-13 13:27:05 +0300

    added def order_forming_complete

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -106,5 +106,12 @@ class OrderRead(DetailView):
         return context
 
 
+def order_forming_complete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order.status = Order.SENT_TO_PROCEED
+    order.save()
+
+    return HttpResponseRedirect(reverse('ordersapp:orders_list'))
+
 
 

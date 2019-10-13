33ef8a4f1f19252a9aeb6d23e18c5d423a7582ad
broocklyn <d.broocklyn@gmail.com>
2019-10-13 12:47:22 +0300

    added OrderItemsCreate class

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -15,3 +15,48 @@ class OrderList(ListView):
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+
+
+class OrderItemsCreate(CreateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:orders_list')
+
+    def get_context_data(self, **kwargs):
+        data = super(OrderItemsCreate, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            basket_items = Basket.get_items(self.request.user)
+            if len(basket_items):
+                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
+                formset = OrderFormSet()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+
+                basket_items.delete()
+            else:
+                formset = OrderFormSet()
+
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        if self.object.get_total_cost() == 0:
+            self.object.delete()
+
+        return super(OrderItemsCreate, self).form_valid(form)
+

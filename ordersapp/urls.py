99f366e0c4e8e241053e7126cde42fb9cc79a110
broocklyn <d.broocklyn@gmail.com>
2019-10-13 13:47:55 +0300

    added order_read url

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     re_path(r'^create/$', ordersapp.OrderItemsCreate.as_view(), name='order_create'),
     re_path(r'^update/(?P<pk>\d+)/$', ordersapp.OrderItemsUpdate.as_view(), name='order_update'),
     re_path(r'^delete/(?P<pk>\d+)/$', ordersapp.OrderDelete.as_view(), name='order_delete'),
-
+    re_path(r'^read/(?P<pk>\d+)/$', ordersapp.OrderRead.as_view(), name='order_read'),
 
 ]

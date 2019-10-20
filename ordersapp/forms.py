d1589507a39e8e81e40762fa4323609494075759
broocklyn <d.broocklyn@gmail.com>
2019-10-20 18:52:26 +0300

    added get_items

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from mainapp.models import Product
 from ordersapp.models import Order, OrderItem
 
 
@@ -12,6 +13,7 @@ class OrderForm(forms.ModelForm):
         super(OrderForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+        self.fields['product'].queryset = Product.get_items()
 
 
 class OrderItemForm(forms.ModelForm):
